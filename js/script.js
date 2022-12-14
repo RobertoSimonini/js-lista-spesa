1039bc5e18323d6140b36f893ce5360608ea02fc
RobertoSimonini <robertosimonini1@gmail.com>
2022-12-14 14:51:12 +0100

    got element from dom and created list array

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-console.log('OK JS')
+
+/*
+    Consegna:
+    Data una lista della spesa (inventatela), stampare in pagina gli elementi della lista individualmente con un ciclo while.  (anche brutalmente, basta che si vedano)
+    Bonus:
+    Provare a stampare sia con i template literals che con i nodi, manipolando il DOM.const 
+*/
+
+// Prendo l'ul dal dom 
+const listContainer = document.getElementById('list-container');
+// Creao l'array con la lista della spesa
 const list = ['uova', 'latte', 'acqua', 'pasta'];
 
+
+
+let i = 0
